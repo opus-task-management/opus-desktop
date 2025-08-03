aa2d399cb432998bc5f2e9c6cedbcdcda00f442a
Raboro <marius.woerfelreal@gmail.com>
2025-08-03 21:02:57 +0200

    refactor(add-task): :recycle: Extract component for add dialog

--- src/AppSidebar.tsx
+++ src/AppSidebar.tsx
@@ -1,4 +1,4 @@
-import { Inbox, Plus, Search, Settings } from "lucide-react";
+import { Inbox, Search } from "lucide-react";
 
 import {
   Sidebar,
@@ -11,19 +11,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { cn } from "./lib/utils";
-import { Button } from "./components/ui/button";
-import { Input } from "./components/ui/input";
-import { useState } from "react";
+import { AddTaskDialog } from "./AddTaskDialog";
 
 const items = [
   {
@@ -43,50 +31,13 @@ interface AppSidebarProps {
 }
 
 export function AppSidebar(props: AppSidebarProps) {
-  const [name, setName] = useState("");
-
-  const addTask = () => props.addTask(name);
-
-  console.log(name);
-
   return (
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Opus</SidebarGroupLabel>
           <SidebarGroupAction>
-            <Dialog>
-              <Settings className={cn("mr-2")} />{" "}
-              <span className="sr-only">Open Settings</span>
-              <DialogTrigger asChild>
-                <Plus className={cn("mr-5")} />
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>Add a task</DialogTitle>
-                </DialogHeader>
-
-                <div className="grid gap-3">
-                  <Input
-                    id="name-1"
-                    name="name"
-                    placeholder="Name"
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                </div>
-
-                <DialogFooter>
-                  <DialogClose asChild>
-                    <Button variant="outline">Cancel</Button>
-                  </DialogClose>
-                  <DialogClose>
-                    <Button type="submit" onClick={addTask}>
-                      Add
-                    </Button>
-                  </DialogClose>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
+            <AddTaskDialog addTask={props.addTask} />
           </SidebarGroupAction>
           <SidebarGroupContent>
             <SidebarMenu>
