2ea0e79a029409598f95024d2f98c60b18f9e50c
Raboro <marius.woerfelreal@gmail.com>
2025-09-08 16:01:19 +0200

    feat(search): :sparkles: Add search dialog component

---
+++ src/SearchTaskDialog.tsx
@@ -0,0 +1,66 @@
+import { Search } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./components/ui/dialog";
+import { Input } from "./components/ui/input";
+import { SidebarMenuButton } from "./components/ui/sidebar";
+import { Task } from "./models/task";
+import { useEffect, useState } from "react";
+
+interface SearchTaskDialogProps {
+  searchForTask: (searchTerm: string) => Task[];
+}
+
+export function SearchTaskDialog(props: SearchTaskDialogProps) {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  useEffect(() => {
+    setTasks(props.searchForTask(searchTerm));
+  }, [searchTerm, props]);
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <SidebarMenuButton asChild>
+          <a href={"#"}>
+            <Search />
+            <span>Search</span>
+          </a>
+        </SidebarMenuButton>
+      </DialogTrigger>
+      <DialogContent>
+        <div className="min-h-100">
+            <DialogHeader>
+            <DialogTitle>Search</DialogTitle>
+            </DialogHeader>
+
+            <div className="grid gap-3 mt-5">
+            <Input
+                id="search-1"
+                name="search"
+                placeholder="Name"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+
+            <div className="mt-4 max-h-100 overflow-y-auto pr-2">
+                {tasks.map((task) => (
+                    <div
+                    key={`task-container-${task.id}`}
+                    className="flex items-start gap-3 w-full border-b py-2"
+                    >
+                    <p>{task.content}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
